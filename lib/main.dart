728bc008f6d03caac1547bcee2b835a77334c03e
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-31 21:02:52 +0100

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: const BallPage(),
+      ),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
