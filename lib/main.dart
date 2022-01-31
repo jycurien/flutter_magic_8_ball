894c73890be668619d8a09cd1f9f3b05bd2aa323
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-31 21:13:28 +0100

    add randomness

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -32,12 +33,20 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Image.asset('images/ball1.png'),
+        child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png')),
       ),
     );
   }
